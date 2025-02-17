aa4954cc78d66573321e6cde80a0e340a181023e
Matheus Correa <matheus.barrosc@gmail.com>
2025-02-17 07:47:50 -0300

    Ajuste na lista de date_filter a qual estava com erro para filtrar corretamente. Junção de algumas rodadas dos playoffs.

--- main.py
+++ main.py
@@ -5,34 +5,27 @@ import analysis
 lol_csv_path = "extras/2024_LoL_esports_match_data_from_OraclesElixir.csv"
 
 date_filter_list = [
-    "2024-01-20",  # Semana 1, Rodada 1
-    "2024-01-21",  # Semana 1, Rodada 2
-    "2024-01-27",  # Semana 2, Rodada 3
-    "2024-01-28",  # Semana 2, Rodada 4
-    "2024-02-03",  # Semana 3, Rodada 5
-    "2024-02-04",  # Semana 3, Rodada 6
-    "2024-02-10",  # Semana 4, Rodada 7
-    "2024-02-11",  # Semana 4, Rodada 8
-    "2024-02-17",  # Semana 5, Rodada 9
-    "2024-02-18",  # Semana 5, Rodada 10
-    "2024-02-24",  # Semana 6, Rodada 11
-    "2024-02-25",  # Semana 6, Rodada 12
-    "2024-03-02",  # Semana 7, Rodada 13
-    "2024-03-03",  # Semana 7, Rodada 14
-    "2024-03-09",  # Semana 8, Rodada 15
-    "2024-03-10",  # Semana 8, Rodada 16
-    "2024-03-16",  # Semana 9, Rodada 17
-    "2024-03-17",  # Semana 9, Rodada 18
-    ["2024-03-22",  # Fase Eliminatória, Rodada 1 Chave Superior [Série 1]
-    "2024-03-23"],  # Fase Eliminatória, Rodada 1 Chave Superior [Série 2]
-    ["2024-03-30",  # Fase Eliminatória, Semifinal Chave Superior [Série 3]
-    "2024-03-31"],  # Fase Eliminatória, Semifinal Chave Superior [Série 4]
-    ["2024-04-05",  # Fase Eliminatória, Rodada 1 Chave Inferior [Série 5]
-    "2024-04-06"],  # Fase Eliminatória, Rodada 1 Chave Inferior [Série 6]
-    "2024-04-07",  # Fase Eliminatória, Final da Chave Superior [Série 7]
-    "2024-04-13",  # Fase Eliminatória, Rodada 2 Chave Inferior [Série 8]
-    "2024-04-14",  # Fase Eliminatória, Final da Chave Inferior [Série 9]
-    "2024-04-20"   # Fase Eliminatória, Grande Final [Série 10]
+    ["2024-01-20"],  # Semana 1, Rodada 1
+    ["2024-01-21"],  # Semana 1, Rodada 2
+    ["2024-01-27"],  # Semana 2, Rodada 3
+    ["2024-01-28"],  # Semana 2, Rodada 4
+    ["2024-02-03"],  # Semana 3, Rodada 5
+    ["2024-02-04"],  # Semana 3, Rodada 6
+    ["2024-02-10"],  # Semana 4, Rodada 7
+    ["2024-02-11"],  # Semana 4, Rodada 8
+    ["2024-02-17"],  # Semana 5, Rodada 9
+    ["2024-02-18"],  # Semana 5, Rodada 10
+    ["2024-02-24"],  # Semana 6, Rodada 11
+    ["2024-02-25"],  # Semana 6, Rodada 12
+    ["2024-03-02"],  # Semana 7, Rodada 13
+    ["2024-03-03"],  # Semana 7, Rodada 14
+    ["2024-03-09"],  # Semana 8, Rodada 15
+    ["2024-03-10"],  # Semana 8, Rodada 16
+    ["2024-03-16"],  # Semana 9, Rodada 17
+    ["2024-03-17"],  # Semana 9, Rodada 18
+    ["2024-03-22", "2024-03-23", "2024-03-30", "2024-03-31", "2024-04-07"],  # Upper Bracket
+    ["2024-04-05", "2024-04-06", "2024-04-13", "2024-04-14"],  # Lower Bracket
+    ["2024-04-20"]   # Fase Eliminatória, Grande Final
 ]
 
 rounds_list = [
@@ -54,12 +47,8 @@ rounds_list = [
     "Rodada 16",
     "Rodada 17",
     "Rodada 18",
-    "Rodada 1 Chave Superior",
-    "Semifinal Chave Superior",
-    "Rodada 1 Chave Inferior",
-    "Final da Chave Superior",
-    "Rodada 2 Chave Inferior",
-    "Final da Chave Inferior",
+    "Chave Superior",
+    "Chave Inferior",
     "Grande Final"
 ]
 
